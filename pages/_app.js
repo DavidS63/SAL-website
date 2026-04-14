2abe9095c1804fd35c645eb93ef78abac8d3704e
David <lisa@sunshineafterloss.com>
2026-04-13 19:06:11 -0700

    Add Clerk authentication

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
 import '../styles/globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <ClerkProvider {...pageProps}>
       <Header />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </ClerkProvider>
   )
 }

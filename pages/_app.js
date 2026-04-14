8eed6c3432000316056282259d689ef8904fe52a
David <lisa@sunshineafterloss.com>
2026-04-13 20:47:06 -0700

    Update Next.js to latest secure version

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import '../styles/globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
+import { useRouter } from 'next/router'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
   return (
-    <ClerkProvider {...pageProps}>
+    <ClerkProvider
+      {...pageProps}
+      routerPush={(to) => router.push(to)}
+      routerReplace={(to) => router.replace(to)}
+    >
       <Header />
       <Component {...pageProps} />
       <Footer />

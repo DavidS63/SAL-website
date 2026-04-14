064674709d27c0a8073016d3f48663aa33be5996
David <lisa@sunshineafterloss.com>
2026-04-13 21:19:03 -0700

    Clean working site no Clerk

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,13 @@
-import '../styles/globals.css'
-import { ClerkProvider } from '@clerk/nextjs'
-import { useRouter } from 'next/router'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-
-export default function App({ Component, pageProps }) {
-  const router = useRouter()
-  return (
-    <ClerkProvider
-      {...pageProps}
-      routerPush={(to) => router.push(to)}
-      routerReplace={(to) => router.replace(to)}
-    >
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
-    </ClerkProvider>
-  )
-}
+import '../styles/globals.css' 
+import Header from '../components/Header' 
+import Footer from '../components/Footer' 
+ 
+export default function App({ Component, pageProps }) { 
+  return ( 
+    <> 
+      <Header /> 
+      <Component {...pageProps} /> 
+      <Footer /> 
+    </> 
+  ) 
+} 
